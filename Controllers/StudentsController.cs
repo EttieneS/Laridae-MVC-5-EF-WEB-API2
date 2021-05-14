7004b33e86160b11eb314566c8c048a30fd631c1
EttieneS <christoffelbothma@gmail.com>
2021-05-14 15:16:01 +0200

    Students.Name crud

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laeridae_API.Models;
+
+namespace Laeridae.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(Students student)
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
